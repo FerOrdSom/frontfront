2e414996070586ef6f6462951cab972d5914dfa3
FerOrdSom <fordsom@gmail.com>
2023-05-28 00:35:02 +0200

    little refactor on moving ball

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
 class Ball{
     constructor(x, y, radius){
         this.positionX = x;
         this.positionY = y;
         this.radius = radius;
     }
-
     getX(){
         return this.positionX;
     }
@@ -31,60 +29,33 @@ class Ball{
     }
 }
 class Renderer{
-    constructor(objects){
+    constructor(){
         this.canvas = document.getElementById("canvas");
-        this.ctx = canvas.getContext("2d");
-        this.objects = objects;
+        this.ctx = canvas.getContext("2d");        
     }
-
-    render(){
-        for (let i = 0; i < this.objects.length; i++){
-            this.objects[i].render(this.canvas);
+    render(objects){
+        for (let i = 0; i < objects.length; i++){
+            objects[i].render(this.canvas);
         }
     }
     refreshScreen(){
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-
 }
-
-
-
-window.addEventListener("load", ()=>{
-
-    var resources = inicialize();
-    console.log(resources);    
+window.addEventListener("load", ()=>{    
+    var resources = [new Ball(400, 300, 10)];
+    var renderer = new Renderer();
     function loop() {
-        resources.renderer.refreshScreen();
-        print();
-        console.log(resources.renderer);
-        step(resources.renderer.objects);//step in simulation
-        console.log(resources.renderer.objects[0].getX());
-        console.log(resources.renderer.objects[0].getY());
-        console.log(resources.renderer.objects);
-        resources.renderer.render(); //render results of step
+        renderer.refreshScreen();//clear screen       
+        step(resources);//step in simulation        
+        renderer.render(resources); //render results of step
         setTimeout(loop,16);//request next step in 1000ms    
     }
-    function print(){
-        resources.count++;
-        console.log(`count: ${resources.count}`);    
-    }
-    
-    // console.log(resources);
-    // console.log(resources.ball);
     loop();
 });
-//Most basic game loop
-function step(objetos){
-    console.log(objetos);
+
+function step(objetos){    
     objetos[0].setX(objetos[0].getX() + 2);
     objetos[0].setY(objetos[0].getY() + 1);
 }
-// Creates resources
-function inicialize(){
-    var count = 0 //state of game
-    var objects = [new Ball(400, 300, 10)]; //state
-    var renderer = new Renderer(objects); //state
-    var status = {"count": count, "renderer": renderer};
-    return status;
-}
+
