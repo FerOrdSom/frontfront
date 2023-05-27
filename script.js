de07a371c189e44a279983261f12f6d6c5c0cbc0
FerOrdSom <fordsom@gmail.com>
2023-05-27 19:36:29 +0200

    simplest game loop

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ window.addEventListener("load", ()=>{
     ctx.fillRect(400, 300, 10, 10);
     console.log(`canvas width: ${canvas.width} canvas height: ${canvas.height}`)    
 });
+//Most basic game loop
+var count = 0 //state of game
+const loop = ()=>{     
+    print();//step in simulation
+    setTimeout(loop,1000);//request next step in 1000ms
+}
+var print = ()=>{
+    count++;
+    console.log(`count: ${count}`);    
+}
+loop();
